0ebb210e729a9ff82daf7d974e9012a63a1c9d0c
Georgii Sh <kelariss@gmail.com>
2020-02-12 18:05:23 +0700

    Fixed test

--- src/containers/GameContainer/useGame.test.ts
+++ src/containers/GameContainer/useGame.test.ts
@@ -14,14 +14,16 @@ describe('useGame', () => {
     expect(rounds).toStrictEqual([[2, 3]]);
   });
 
-  test('should return correct round with roll 10 and 2', () => {
+  test('should return correct round with roll 10 and 2', async () => {
     const { result } = renderHook(() => useGame());
-    const [rounds, roll] = result.current;
+    const [, roll] = result.current;
 
     act(() => {
       roll(10);
       roll(2);
     });
+
+    const [rounds] = result.current;
 
     expect(rounds).toStrictEqual([[10], [2]]);
   });
